dd80bc0e72591306ec9e4cfa5e4cbb205513f7cb
ShinooGoyal <shinoogoyal@gmail.com>
2017-06-09 14:47:32 +0530

    TryCrahslytics sample
    Trycrahslytics sample app

---
+++ app/src/androidTest/java/io/fabric/atry/com/trycrashlytics/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.fabric.atry.com.trycrashlytics;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/io/fabric/atry/com/trycrashlytics/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package io.fabric.atry.com.trycrashlytics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
